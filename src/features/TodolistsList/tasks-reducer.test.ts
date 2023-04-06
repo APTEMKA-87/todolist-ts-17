be888a95e21a06aa3d9e53d3c8d9067c9f7f6eeb
APTEMKA-87 <1274646a1@gmail.com>
2023-04-06 20:55:37 +0300

    fix test

--- src/features/TodolistsList/tasks-reducer.test.ts
+++ src/features/TodolistsList/tasks-reducer.test.ts
@@ -33,7 +33,7 @@ beforeEach(() => {
 });
 
 test('correct task should be deleted from correct array', () => {
-    const action = removeTaskAC("2", "todolistId2");
+    const action = removeTaskAC({taskId: "2",todolistId: "todolistId2"});
 
     const endState = tasksReducer(startState, action)
 
@@ -44,16 +44,18 @@ test('correct task should be deleted from correct array', () => {
 test('correct task should be added to correct array', () => {
     //const action = addTaskAC("juce", "todolistId2");
     const action = addTaskAC({
-        todoListId: "todolistId2",
-        title: "juce",
-        status: TaskStatuses.New,
-        addedDate: "",
-        deadline: "",
-        description: "",
-        order: 0,
-        priority: 0,
-        startDate: "",
-        id: "id exists"
+        task: {
+            todoListId: "todolistId2",
+            title: "juce",
+            status: TaskStatuses.New,
+            addedDate: "",
+            deadline: "",
+            description: "",
+            order: 0,
+            priority: 0,
+            startDate: "",
+            id: "id exists"
+        }
     });
 
     const endState = tasksReducer(startState, action)
@@ -65,7 +67,7 @@ test('correct task should be added to correct array', () => {
     expect(endState["todolistId2"][0].status).toBe(TaskStatuses.New);
 });
 test('status of specified task should be changed', () => {
-    const action = updateTaskAC("2", {status: TaskStatuses.New}, "todolistId2");
+    const action = updateTaskAC({taskId:"2",model: {status: TaskStatuses.New},todolistId: "todolistId2"});
 
     const endState = tasksReducer(startState, action)
 
@@ -73,7 +75,7 @@ test('status of specified task should be changed', () => {
     expect(endState["todolistId2"][1].status).toBe(TaskStatuses.New);
 });
 test('title of specified task should be changed', () => {
-    const action = updateTaskAC("2", {title: "yogurt"}, "todolistId2");
+    const action = updateTaskAC({taskId: "2",model: {title: "yogurt"},todolistId: "todolistId2"});
 
     const endState = tasksReducer(startState, action)
 
@@ -83,17 +85,19 @@ test('title of specified task should be changed', () => {
 });
 test('new array should be added when new todolist is added', () => {
     const action = addTodolistAC({
-        id: "blabla",
-        title: "new todolist",
-        order: 0,
-        addedDate: ''
+        todolist: {
+            id: "blabla",
+            title: "new todolist",
+            order: 0,
+            addedDate: ''
+        }
     });
 
     const endState = tasksReducer(startState, action)
 
 
     const keys = Object.keys(endState);
-    const newKey = keys.find(k => k != "todolistId1" && k != "todolistId2");
+    const newKey = keys.find(k => k !== "todolistId1" && k !== "todolistId2");
     if (!newKey) {
         throw Error("new key should be added")
     }
@@ -113,10 +117,12 @@ test('propertry with todolistId should be deleted', () => {
 });
 
 test('empty arrays should be added when we set todolists', () => {
-    const action = setTodolistsAC([
-        {id: "1", title: "title 1", order: 0, addedDate: ""},
-        {id: "2", title: "title 2", order: 0, addedDate: ""}
-    ])
+    const action = setTodolistsAC({
+        todolists: [
+            {id: "1", title: "title 1", order: 0, addedDate: ""},
+            {id: "2", title: "title 2", order: 0, addedDate: ""}
+        ]
+    })
 
     const endState = tasksReducer({}, action)
 
@@ -127,7 +133,7 @@ test('empty arrays should be added when we set todolists', () => {
     expect(endState['2']).toBeDefined()
 })
 test('tasks should be added for todolist', () => {
-    const action = setTasksAC(startState["todolistId1"], "todolistId1");
+    const action = setTasksAC({tasks: startState["todolistId1"],todolistId: "todolistId1"});
 
     const endState = tasksReducer({
         "todolistId2": [],
